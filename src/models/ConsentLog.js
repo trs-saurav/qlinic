5b0189fe8fa94c7c56ae8edf3cd85a2ac55c3aa8
Saurav <saurv123@outlook.com>
2026-02-06 16:18:16 +0530

    consent fix

--- src/models/ConsentLog.js
+++ src/models/ConsentLog.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const ConsentLogSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.Schema.Types.ObjectId, 
+    type: String, 
     ref: 'User', 
     required: true,
     index: true
